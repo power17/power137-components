bf485f392a50d24aaa3f3ae2fd2f321219dc9e4b
power17 <1410837981@qq.com>
2023-12-16 00:15:40 +0800

    feat: checkEllipsis

--- src/components/EllipsisText.vue
+++ src/components/EllipsisText.vue
@@ -1,8 +1,9 @@
 <template>
-  <div ref="ellipsisRef" class="ellipsis box">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+  <div ref="ellipsisRef" @mouseenter="checkEllisis" class="ellipsis box">Lorem ipsum dolor sit amet consectetur123123</div>
+  <div>{{ result }}</div>
 </template>
 <script lang="ts" setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, Ref } from 'vue';
 const getPadding = (el: HTMLElement) => {
   const sytle = window.getComputedStyle(el, null);
   const paddingLeft = Number.parseInt(sytle.paddingLeft, 10) || 0;
@@ -10,28 +11,36 @@ const getPadding = (el: HTMLElement) => {
   const paddingTop = Number.parseInt(sytle.paddingTop, 10) || 0;
   const paddingBottom = Number.parseInt(sytle.paddingBottom, 10) || 0;
   return {
-    left: paddingLeft,
-    right: paddingright,
+    pLeft: paddingLeft,
+    pRight: paddingright,
     top: paddingTop,
     bottom: paddingBottom,
   };
 };
-
-const checkEllisis = (el: HTMLElement) => {
+const result = ref('null');
+const checkEllisis = (e: MouseEvent) => {
+  const box = e.target as HTMLElement;
   const range = document.createRange();
-  range.setStart(el, 0);
-  range.setEnd(el, el.childNodes.length);
-  console.log(range, el.childNodes);
+  range.setStart(box, 0);
+  range.setEnd(box, box.childNodes.length);
+  let rangeWidth = range.getBoundingClientRect().width;
+  // let rangeHeight = range.getBoundingClientRect().height;
+  // const contentWidth = rangeWidth - Math.floor(rangeWidth);
+  const { pLeft, pRight } = getPadding(box);
+  const horizontalPadding = pLeft + pRight;
+  if (rangeWidth + horizontalPadding > box.clientWidth) {
+    console.log('存在省略号');
+  } else {
+    console.log('容器宽度足够，没有省略号了');
+  }
 };
 const ellipsisRef = ref(null);
-onMounted(() => {
-  if (ellipsisRef.value) {
-    checkEllisis(ellipsisRef.value);
-  }
-});
 </script>
 
 <style scoped>
+.box {
+  width: 300px;
+}
 .ellipsis {
   overflow: hidden;
   text-overflow: ellipsis;
