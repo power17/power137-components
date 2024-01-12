016f7147ce3fdec4c40a3eee01cebd98dbf2b33a
power17 <1410837981@qq.com>
2024-01-13 00:26:23 +0800

    feat: add vitest

---
+++ src/components/Test.test.ts
@@ -0,0 +1,8 @@
+import { test } from 'vitest';
+import { render } from '@testing-library/vue';
+import Test from './Test';
+test('it should work', () => {
+  const { getByText } = render(Test, { props: { msg: 'hello' } });
+  // assert output
+  getByText('hello');
+});
--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,1 +1,15 @@
-export default () => <div>test</div>;
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'STest',
+  props: {
+    msg: {
+      type: String,
+      default: 'test'
+    }
+  },
+  setup(props) {
+    return () => {
+      return <div>{props.msg}</div>;
+    };
+  }
+});
