796ebe1a12e19d38556de90b6a63573727f0cbf3
power17 <1410837981@qq.com>
2024-01-19 23:47:33 +0800

    feat: add  cli type

--- cli/src/commands/create.ts
+++ cli/src/commands/create.ts
@@ -1,3 +1,26 @@
-export const onCreate = (cmd: string) => {
-  console.log(cmd);
+import inquirer from 'inquirer';
+import { red } from 'kolorist';
+const CREATE_TYPES = ['component', 'lib'];
+export const onCreate = async (cmd: { [key: string]: string }) => {
+  let { type } = cmd;
+  if (!type) {
+    const result = await inquirer.prompt([
+      {
+        name: 'type',
+        message: '必填）请选择创建类型:',
+        choices: CREATE_TYPES,
+        default: 0 // CREATE_TYPE index
+      }
+    ]);
+    type = result.type;
+  }
+  if (!CREATE_TYPES.includes(type)) {
+    console.log(
+      red(
+        `当前类型仅支持：${CREATE_TYPES.join(
+          ', '
+        )}，收到不在支持范围内的 "${type}"`
+      )
+    );
+  }
 };
--- cli/src/index.ts
+++ cli/src/index.ts
@@ -4,6 +4,6 @@ const program = new Command();
 program
   .command('create')
   .description('创建一个组件模板或配置文件')
-  .option('-t --type <type>', `创建类型，可选值：component, lib-entry`)
+  .option('-t --type <type>', `创建类型,可选值:component, lib-entry`)
   .action(onCreate);
 program.parse();
